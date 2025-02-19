d19028d872e3d2e2df9bd1164b5d4a31393a4726
Alicombot <alicombotpc@gmail.com>
2025-02-19 22:35:30 +0330

    content-show of menu-item & fixed bug close sidebar & hide active-menuItem time closing

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,41 @@
 const menuList = document.querySelectorAll(".menu-list__item");
-const panelShow = document.querySelector(".panel--show");
 const managelistWrapper = document.querySelectorAll(".manage-list__wrapper");
 const chevronRight = document.querySelectorAll(".chevron-right");
 const panelthemChange = document.querySelectorAll(".panel-them__change");
+const panelContent = document.querySelectorAll(".panel__content");
 
 
-menuList.forEach(menuList =>{
-    menuList.addEventListener('click', function(){
-        document.querySelector(".menu-list__item--active").classList.remove("menu-list__item--active")
-        document.querySelector(".panel").classList.toggle("panel--close")
-        document.querySelector(".wrapper").classList.toggle("wrapper--close")
-        this.classList.add('menu-list__item--active')
-    })
-})
+menuList.forEach(item => {
+    item.addEventListener('click', function() {
+        let contentId = this.getAttribute("data-content-id");
+        
+        
+        document.querySelector(".menu-list__item--active")?.classList.remove("menu-list__item--active");
+        document.querySelector(".panel__content.content--show")?.classList.remove("content--show");
+
+        
+        document.querySelector(".panel").classList.toggle("panel--close");
+        document.querySelector(".wrapper").classList.toggle("wrapper--close");
+
+        
+        this.classList.add('menu-list__item--active');
+
+
+
+        let contentElement = document.querySelector(contentId);
+        if (contentElement && !contentElement.classList.contains("content--show")) {
+            contentElement.classList.add("content--show");
+        }
+    });
+});
+
+document.querySelector(".panel").addEventListener("transitionend", () => {
+    if (document.querySelector(".panel").classList.contains("panel--close")) {
+        document.querySelector(".menu-list__item--active")?.classList.remove("menu-list__item--active");
+        document.querySelector(".panel__content.content--show")?.classList.remove("content--show");
+    }
+});
+
 
 
 managelistWrapper.forEach(managelistWrapper => {
@@ -22,12 +45,11 @@ managelistWrapper.forEach(managelistWrapper => {
 })
 
 
-
-
-console.log(panelthemChange)
 panelthemChange.forEach(panelthemChange => {
     panelthemChange.addEventListener('click', function(){
         document.querySelector(".panel-them--active").classList.remove("panel-them--active")
         this.classList.add("panel-them--active")
     })
 })
+
+
